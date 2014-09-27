79dc7672fed4cc54ad7afb4ca70c786408afc6c4
Andrew Clarkson <andrewclarkson@live.com>
2014-09-26 21:45:44 -0500

    Refactor to separate views

--- app/users/__init__.py
+++ app/users/__init__.py
@@ -1,11 +1,5 @@
 from flask import Blueprint
-from app.users.models import User
 
 users = Blueprint("users", __name__)
 
-@users.route("")
-def index():
-    """
-    Return a page of users
-    """
-    return "TODO"
+from app.users.views import *
---
+++ app/users/views.py
@@ -0,0 +1,14 @@
+from flask import request, render_template
+from app.users import users
+from app.users.models import User
+
+@users.route("", methods=["GET"])
+@users.route("/<int:page>", methods=["GET"])
+def index(page=1):
+    """
+    Returns a list of users
+    """
+    
+    users = User.query.limit(10).all()    
+
+    return render_template("users/index.html", users=users)
